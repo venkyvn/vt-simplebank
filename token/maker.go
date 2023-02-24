e648dfdb07f4cbceb93f919f859959a2e83a742b
VuongTran <khanhvuong939@gmail.com>
2023-02-24 17:33:40 +0700

    implement jwt and paseto mechanism to authenticate user

---
+++ token/maker.go
@@ -0,0 +1,12 @@
+package token
+
+import "time"
+
+// Maker is an interface for managing token, we can choose which technology (JWT, PASETO) to use
+type Maker interface {
+	// CreateToken creates a new token for specific username and duration
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken check token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
---
+++ token/payload.go
@@ -0,0 +1,44 @@
+package token
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payload data of a token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// Valid checks if the token is valid or not
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	id, err := uuid.NewUUID()
+
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        id,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
