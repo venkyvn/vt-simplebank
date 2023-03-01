c4339779f9b3b7bed4e119f4f99f0bca62895a4a
VuongTran <khanhvuong939@gmail.com>
2023-03-01 14:39:30 +0700

    load local env before running db migration

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ set -e
 # migrate db first
 #migrate -path db/migration -database "postgresql://root:123@localhost:5432/simple_bank?sslmode=disable" -verbose up
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/db/migration -database "$DB_SOURCE" -verbose up
 
 
