0df36bcb83c8460605bc9678ffb6906a6cfa2adc
VuongTran <khanhvuong939@gmail.com>
2023-03-12 17:07:47 +0700

    implement refresh and renew token.

---
+++ db/migration/000003_add_sessions.down.sql
@@ -0,0 +1,1 @@
+alter table if exists "sessions" drop constraint if exists "owner_currency_key";
--- token/maker.go
+++ token/maker.go
@@ -5,7 +5,7 @@ import "time"
 // Maker is an interface for managing token, we can choose which technology (JWT, PASETO) to use
 type Maker interface {
 	// CreateToken creates a new token for specific username and duration
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 
 	// VerifyToken check token is valid or not
 	VerifyToken(token string) (*Payload, error)
