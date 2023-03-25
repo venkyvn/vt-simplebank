4a2ec956a135726e877f64090ee212c5f5b4bdb8
VuongTran <khanhvuong939@gmail.com>
2023-03-25 11:10:11 +0700

    Implement grpc gateway to handle HTTP request

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 	"log"
 	"net"
+	"net/http"
 	"vuongtran/learning/simplebank/api"
 	db "vuongtran/learning/simplebank/db/sqlc"
 	"vuongtran/learning/simplebank/gapi"
-	simplebank "vuongtran/learning/simplebank/pb"
+	"vuongtran/learning/simplebank/pb"
 	"vuongtran/learning/simplebank/util"
 )
 
@@ -34,6 +38,8 @@ func main() {
 
 	store := db.NewStore(conn)
 
+	go runGatewayServer(config, store)
+
 	runGrpcServer(config, store)
 }
 
@@ -43,7 +49,7 @@ func runGrpcServer(config util.Config, store db.Store) {
 		log.Fatal("cannot create server", err)
 	}
 	grpcServer := grpc.NewServer()
-	simplebank.RegisterSimpleBankServer(grpcServer, server)
+	pb.RegisterSimpleBankServer(grpcServer, server)
 	reflection.Register(grpcServer) // important -> self document to help internal server can find each other
 
 	listener, err := net.Listen("tcp", config.GRPCServerAddress)
@@ -56,6 +62,47 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Fatal("cannot create gRPC server", err)
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(store, config)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server", err)
+	}
+
+	// receive HTTP request from client in order to convert it to the grpc request format
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+
+	err = http.Serve(listener, mux)
+	if err != nil {
+		log.Fatal("cannot create gRPC gateway server", err)
 	}
 }
 
