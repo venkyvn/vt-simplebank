1f8cc56c3b41a94df85fdb92fa09746cb289aa24
VuongTran <khanhvuong939@gmail.com>
2023-03-26 15:32:41 +0700

    embedded the front end file into backend execution binary file

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
+	"github.com/rakyll/statik/fs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/encoding/protojson"
@@ -13,6 +14,7 @@ import (
 	"net/http"
 	"vuongtran/learning/simplebank/api"
 	db "vuongtran/learning/simplebank/db/sqlc"
+	_ "vuongtran/learning/simplebank/doc/statik"
 	"vuongtran/learning/simplebank/gapi"
 	"vuongtran/learning/simplebank/pb"
 	"vuongtran/learning/simplebank/util"
@@ -93,8 +95,13 @@ func runGatewayServer(config util.Config, store db.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
-	fs := http.FileServer(http.Dir("./doc/swagger"))
-	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal("cannot load create statik fs:", err)
+	}
+
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	mux.Handle("/swagger/", swaggerHandler)
 
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
