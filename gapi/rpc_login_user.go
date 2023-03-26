9a4252384aa8759d6b1764906c9bd2cd37c38d13
VuongTran <khanhvuong939@gmail.com>
2023-03-26 16:07:18 +0700

    standardize the response error

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -3,15 +3,21 @@ package gapi
 import (
 	"context"
 	"database/sql"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	db "vuongtran/learning/simplebank/db/sqlc"
 	"vuongtran/learning/simplebank/pb"
 	"vuongtran/learning/simplebank/util"
+	"vuongtran/learning/simplebank/val"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	if violations := validateLoginUserRequest(req); violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -59,4 +65,15 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
